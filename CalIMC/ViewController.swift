5af29ee1a68d9f7a4cb854c3b5e26ab9d9a37d86
Josue Ruiz <JosueRuiz.com@gmail.com>
2024-12-11 20:14:30 +0100

    terminado

--- CalIMC/ViewController.swift
+++ CalIMC/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var heigth: Int = 160;
+    var weigth: Float = 60.0;
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Altura //
+    
+    @IBOutlet weak var heigthLabel: UILabel!
+    @IBAction func setHeight(_ sender: UISlider) {
+        heigth = Int (sender.value);
+        heigthLabel.text = "\(heigth) cm";
+    }
+    
+    // Peso //
+    
+    @IBOutlet weak var weigthLabel: UILabel!
+    @IBAction func setWeight(_ sender: UIStepper) {
+        weigth = Float(sender.value);
+        weigthLabel.text = "\(weigth) Kg";
+    }
+    
+    // Calcular //
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func calculate(_ sender: Any) {
+        let hinmeters = (Float(heigth) / 100.0);
+        let imc = weigth / pow(hinmeters, 2);
+        resultLabel.text = String(format: "%.2f", imc);
+    }
 }
 
