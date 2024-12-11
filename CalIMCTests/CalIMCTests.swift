2b807931d07ef9c340f6063afe75eaddc289477d
Josue Ruiz <JosueRuiz.com@gmail.com>
2024-12-11 16:45:51 +0100

    Initial Commit

---
+++ CalIMCTests/CalIMCTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalIMCTests.swift
+//  CalIMCTests
+//
+//  Created by Tardes on 11/12/24.
+//
+
+import Testing
+@testable import CalIMC
+
+struct CalIMCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
